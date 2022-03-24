b85b65c6681699531cbd21b4331828b48e166ac9
Tobias Lindström <me@tobiaslindstrom.se>
2022-03-24 21:29:32 +0100

    Added a helper function to get the filename and line of the calling function.

--- helper.js
+++ helper.js
@@ -28,6 +28,19 @@ const addImage = (imageBuffer) => {
   console.log(`addImage, imageBuffer: ${imageBuffer}, testPath: ${testPath}, testName: ${testName}`)
 }
 
+const getCalledLine = () => {
+  const error = new Error()
+  const callerLine = error.stack.split("\n")[2]
+  const regex = /\((.*):(\d+):(\d+)\)$/
+  const match = regex.exec(callerLine)
+
+  return {
+    file: match[1],
+    line: parseInt(match[2], 10)
+  };
+}
+
 module.exports.addMessage = addMessage
 module.exports.addImage = addImage
+module.exports.getCalledLine = getCalledLine
 
--- test/helper.test.js
+++ test/helper.test.js
@@ -1,3 +1,10 @@
-test.skip('TODO: Add helper tests', async () => {
-  expect(true).to.equal(true)
+const { getCalledLine } = require('../helper')
+
+test('helper function to get the filename and line of the function calling it', () => {
+  const calledLine = getCalledLine()
+
+  expect(calledLine).toEqual({
+    file: `${process.cwd()}/test/helper.test.js`,
+    line: 4,
+  })
 })
